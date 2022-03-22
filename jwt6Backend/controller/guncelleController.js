2b8b0f18cbfb6900021eb4b36a907b908443c51e
ahmetyanik <ahmetyank4242@gmail.com>
2022-03-22 21:35:02 +0100

    ready

--- jwt6Backend/controller/guncelleController.js
+++ jwt6Backend/controller/guncelleController.js
@@ -1,8 +1,7 @@
-async function guncelleController(req,res){
-
-    res.json({
-        message:"Here is secret page!"
-    })
+async function guncelleController(req, res) {
+  res.json({
+    message: "Here is secret page!",
+  });
 }
 
 module.exports = guncelleController;
--- jwt6Backend/middleware/auth.js
+++ jwt6Backend/middleware/auth.js
@@ -1,20 +1,15 @@
 const jwt = require("jsonwebtoken");
 
+module.exports = async (req, res, next) => {
+  const authFromHeader = req.get("Authorization");
 
-module.exports = async (req,res,next)=>{
+  const token = authFromHeader.split(" ")[1];
 
-    const authFromHeader = req.get("Authorization");
+  const dogrula = jwt.verify(token, "sifre");
 
-    const token = authFromHeader.split(" ")[1];
-
-    const dogrula = jwt.verify(token,"sifre");
-
-    if(dogrula){
-        next();
-    }else{
-        next("Token is not valid!")
-    }
-
-
-
-}
+  if (dogrula) {
+    next();
+  } else {
+    next("Token is not valid!");
+  }
+};
